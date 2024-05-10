227718689918fff415571323580810621ebf912a
MeetMalvaniya <meetmalvaniya2004@gmail.com>
2024-05-10 23:13:10 +0530

    Create loginsignup.py

---
+++ loginsignup.py
@@ -0,0 +1,14 @@
+print("Create Account")
+username = input("Enter Username :")
+password = input("Enter Password :")
+
+print("Your accout was created successfully")
+print("Login Now")
+
+username1 =  input("Enter Username :")
+password1 = input("Enter Password :")
+
+if username == username1 and password == password1:
+    print("Login Successful")
+else:
+    print("Login Failed")
